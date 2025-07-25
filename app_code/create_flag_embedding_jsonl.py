ad01d5cc1552bc334778595915a10196ef05423b
Jan Sowa <jan.piotr.sowa@gmail.com>
2025-07-25 12:04:31 +0200

    Add subtraction and multiplication negatives threshold

--- app_code/create_flag_embedding_jsonl.py
+++ app_code/create_flag_embedding_jsonl.py
@@ -10,8 +10,10 @@ def process_negatives(
     negatives_path: str,
     output_path: str,
     num_negatives: int,
-    negatives_threshold: float
+    negatives_multiplication_threshold: float ,
+    negatives_subtraction_threshold: float
 ) -> None:
+
 
     corpus_df = pd.read_parquet(corpus_path)
     queries_df = pd.read_parquet(queries_path)
@@ -43,7 +45,10 @@ def process_negatives(
 
         if qid in negatives_grouped.groups:
             neg_rows = negatives_grouped.get_group(qid)
-            threshold = negatives_threshold * min_positive_score
+            if negatives_multiplication_threshold is not None:
+                threshold = min_positive_score * negatives_multiplication_threshold
+            else:
+                threshold = min_positive_score  - negatives_subtraction_threshold
             filtered_negs = neg_rows[neg_rows["ranking"] < threshold]
             top_negs = filtered_negs.sort_values(by="ranking", ascending=False).head(num_negatives)
 
@@ -74,7 +79,29 @@ if __name__ == "__main__":
     parser.add_argument("--negatives_path", type=str, default=config("NEGATIVES_PATH"), help="Path to parquet file with negatives")
     parser.add_argument("--output_path", type=str, default=config("OUTPUT_PATH"), help="Path to output parquet file")
     parser.add_argument("--num_negatives", type=int, default=config("NUM_NEGATIVES", cast=int), help="Number of negatives for each question")
-    parser.add_argument("--negatives_threshold", type=float, default=config("NEGATIVES_THRESHOLD", cast=float), help="Threshold for negatives (percent of the minimum positive)")
+
+    negatives_subtraction_threshold_str = config("NEGATIVES_SUBTRACTION_THRESHOLD", default=None)
+    if negatives_subtraction_threshold_str is not None and negatives_subtraction_threshold_str != "":
+        negatives_subtraction_threshold = float(negatives_subtraction_threshold_str)
+    else:
+        negatives_subtraction_threshold = None
+
+
+    negatives_multiplication_threshold_str = config("NEGATIVES_MULTIPLICATION_THRESHOLD", default=None)
+    print(f"{negatives_multiplication_threshold_str=}")
+    if negatives_multiplication_threshold_str is not None and negatives_multiplication_threshold_str != "":
+        negatives_multiplication_threshold = float(negatives_multiplication_threshold_str)
+    else:
+        negatives_multiplication_threshold = None
+
+    if negatives_subtraction_threshold is not None and negatives_multiplication_threshold is not None:
+        raise ValueError("You have to choose between subtraction and multiplication threshold.")
+
+    if negatives_subtraction_threshold is None and negatives_multiplication_threshold is None:
+        raise ValueError("You have to fill in the negatives threshold")
+
+    parser.add_argument("--negatives_multiplication_threshold", required=False, type=float, default=negatives_multiplication_threshold, help="Multiplication threshold for negatives (percent of the minimum positive)")
+    parser.add_argument("--negatives_subtraction_threshold", required=False, type=float, default=negatives_subtraction_threshold, help="Subtraction threshold for negatives (this value will be subtracted from the minimum positive)")
     args = parser.parse_args()
 
     process_negatives(
@@ -84,5 +111,6 @@ if __name__ == "__main__":
         negatives_path=args.negatives_path,
         output_path=args.output_path,
         num_negatives=args.num_negatives,
-        negatives_threshold=args.negatives_threshold
+        negatives_multiplication_threshold=args.negatives_multiplication_threshold,
+        negatives_subtraction_threshold=args.negatives_subtraction_threshold
     )
